64553304bde40b425de0acc3c69a64366bd1eaef
AKaanMol <kaanmol@gmail.com>
2023-01-15 01:05:33 +0300

    C ile BookStore uygulamasi(Asp.Net-core(6.0)))
    c# ile hazırlanmış asp.netcore uygulaması(crud işlemleri yapılmıştır.)

---
+++ BookStore/BookStore/Repositories/Abstract/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Models.Domain;
+
+namespace BookStore.Repositories.Abstract
+{
+    public interface IBookService
+    {
+        bool Add(Book model);
+        bool Update(Book model);
+        bool Delete(int id);
+        Book FindById(int id);
+        IEnumerable<Book> GetAll();
+    }
+}
---
+++ BookStore/BookStore/Repositories/Abstract/IGenreService.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Models.Domain;
+
+namespace BookStore.Repositories.Abstract
+{
+    public interface IGenreService
+    {
+        bool Add(Genre model);
+        bool Update(Genre model);
+        bool delete(int id);
+        Genre FindById(int id);
+        IEnumerable<Genre> GetAll();
+    }
+}
